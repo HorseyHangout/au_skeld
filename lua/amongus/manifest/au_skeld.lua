6de370973e1c93dcf2d356f52dc046bbe9d64d42
Eliot Partridge <byte@code.horse>
2020-11-08 22:50:29 -0600

    Add location labels and sabotage buttons to map
    Pending sabotage implementation

--- lua/amongus/manifest/au_skeld.lua
+++ lua/amongus/manifest/au_skeld.lua
@@ -13,72 +13,74 @@ local MANIFEST = {
 
 				Position = Vector(-3576, 1978),
 				Scale = 3.5,
-				Resolution = 4
+				Resolution = 4,
+
+				-- Labels positions are always relative to 1024x1024, despite cropping of overlay
+				Labels = {
+					-- Center
+					{
+						Text = 'area.cafeteria',
+						Position = Vector(550/1024, 90/1024),
+					},
+					{
+						Text = 'area.storage',
+						Position = Vector(520/1024, 417/1024),
+					},
+			
+					-- Left side
+					{
+						Text = 'area.medbay',
+						Position = Vector(350/1024, 185/1024),
+					},
+					{
+						Text = 'area.upperEngine',
+						Position = Vector(160/1024, 110/1024),
+					},
+					{
+						Text = 'area.reactor',
+						Position = Vector(58/1024, 240/1024),
+					},
+					{
+						Text = 'area.security',
+						Position = Vector(250/1024, 240/1024),
+					},
+					{
+						Text = 'area.lowerEngine',
+						Position = Vector(160/1024, 405/1024),
+					},
+					{
+						Text = 'area.electrical',
+						Position = Vector(370/1024, 345/1024),
+					},
+			
+					-- Right side
+					{
+						Text = 'area.weapons',
+						Position = Vector(790/1024, 115/1024),
+					},
+					{
+						Text = 'area.o2',
+						Position = Vector(725/1024, 215/1024),
+					},
+					{
+						Text = 'area.navigation',
+						Position = Vector(975/1024, 260/1024),
+					},
+					{
+						Text = 'area.admin',
+						Position = Vector(670/1024, 310/1024),
+					},
+					{
+						Text = 'area.shields',
+						Position = Vector(785/1024, 440/1024),
+					},
+					{
+						Text = 'area.communications',
+						Position = Vector(660/1024, 500/1024),
+					},
+				},
 			} end
 		end)(),
-	},
-	Labels = {
-		-- Center
-		{
-			Text = 'area.cafeteria',
-			Position = Vector(0, 0),
-		},
-		{
-			Text = 'area.storage',
-			Position = Vector(0, 0),
-		},
-
-		-- Left side
-		{
-			Text = 'area.medbay',
-			Position = Vector(0, 0),
-		},
-		{
-			Text = 'area.upperEngine',
-			Position = Vector(0, 0),
-		},
-		{
-			Text = 'area.reactor',
-			Position = Vector(0, 0),
-		},
-		{
-			Text = 'area.security',
-			Position = Vector(0, 0),
-		},
-		{
-			Text = 'area.lowerEngine',
-			Position = Vector(0, 0),
-		},
-		{
-			Text = 'area.electrical',
-			Position = Vector(0, 0),
-		},
-
-		-- Right side
-		{
-			Text = 'area.weapons',
-			Position = Vector(0, 0),
-		},
-		{
-			Text = 'area.o2',
-			Position = Vector(0, 0),
-		},
-		{
-			Text = 'area.navigation',
-			Position = Vector(0, 0),
-		},
-		{
-			Text = 'area.admin',
-			Position = Vector(0, 0),
-		},
-		{
-			Text = 'area.shields',
-			Position = Vector(0, 0),
-		},
-		{
-			Text = 'area.communications',
-			Position = Vector(0, 0),
-		},
 	},
 	Tasks = {
 		'divertPower',
@@ -106,7 +108,7 @@ local MANIFEST = {
 			UI = (function ()
 				if CLIENT then return {
 					Icon = Material('au/gui/map/sabotage_reactor.png', 'smooth'),
-					Position = Vector(0, 0)
+					Position = Vector(58/1024, 500/1024)
 				} end
 			end)(),
 		},
@@ -114,8 +116,8 @@ local MANIFEST = {
 			Handler = 'lights',
 			UI = (function ()
 				if CLIENT then return {
-					Icon = Material('au/gui/map/sabotage_lights.png', 'smooth'),
-					Position = Vector(0, 0)
+					Icon = Material('au/gui/map/sabotage_electricity.png', 'smooth'),
+					Position = Vector(405/1024, 680/1024)
 				} end
 			end)(),
 		},
@@ -126,7 +128,7 @@ local MANIFEST = {
 			UI = (function ()
 				if CLIENT then return {
 					Icon = Material('au/gui/map/sabotage_o2.png', 'smooth'),
-					Position = Vector(0, 0)
+					Position = Vector(720/1024, 445/1024)
 				} end
 			end)(),
 		},
@@ -135,7 +137,7 @@ local MANIFEST = {
 			UI = (function ()
 				if CLIENT then return {
 					Icon = Material('au/gui/map/sabotage_comms.png', 'smooth'),
-					Position = Vector(0, 0)
+					Position = Vector(660/1024, 960/1024)
 				} end
 			end)(),
 		},
@@ -146,7 +148,16 @@ local MANIFEST = {
 
 --  name    position on map
 local SABOTAGE_DOORS = {
-	['foo'] = Vector(0, 0),
+	-- Center
+	cafeteria = Vector(550/1024, 220/1024),
+	storage = Vector(520/1024, 810/1024),
+
+	-- Left side
+	medbay = Vector(350/1024, 400/1024),
+	upper_engine = Vector(160/1024, 260/1024),
+	security = Vector(250/1024, 500/1024),
+	lower_engine = Vector(160/1024, 790/1024),
+	electrical = Vector(340/1024, 680/1024),
 }
 
 -------------------------------------------
