5400c28bb6adcaeb8b7359293b026d147adb1e9d
Eliot Partridge <byte@code.horse>
2020-11-09 16:48:21 -0600

    Correctly localize vent names
    TODO: figure out why this isn't loading
    
    Closes #23

---
+++ lua/amongus/lang/au_skeld.lua
@@ -0,0 +1,7 @@
+AddCSLuaFile()
+
+local LANG = GAMEMODE.Lang:Get('en')
+
+LANG['vent.reactorSouth'] = 'Vent to ' .. LANG['area.reactor']
+LANG['vent.navigationSouth'] = 'Vent to ' .. LANG['area.navigation']
+LANG['vent.navHallway'] = 'Vent to ' .. LANG['area.navigation'] .. ' hallway'
