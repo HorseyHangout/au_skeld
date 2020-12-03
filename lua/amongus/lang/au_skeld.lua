1e967ff4b3e8becf622d1c02249fbb092a5c0045
Eliot Partridge <byte@code.horse>
2020-12-03 12:06:06 -0600

    Re-use vent.* LANG strings where possible

--- lua/amongus/lang/au_skeld.lua
+++ lua/amongus/lang/au_skeld.lua
@@ -2,6 +2,6 @@ AddCSLuaFile()
 
 local LANG = GAMEMODE.Lang:Get('en')
 
-LANG['vent.reactorSouth'] = 'Vent to ' .. LANG['area.reactor']
-LANG['vent.navigationSouth'] = 'Vent to ' .. LANG['area.navigation']
-LANG['vent.navHallway'] = 'Vent to ' .. LANG['area.navigation'] .. ' hallway'
+LANG['vent.reactorSouth'] = LANG['vent.reactor']
+LANG['vent.navigationSouth'] = LANG['vent.navigation']
+LANG['vent.navHallway'] = LANG['vent.navigation'] .. ' hallway'
