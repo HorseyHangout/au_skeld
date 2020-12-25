975ad6b48f3e5d71296356f39df1ca0690ebed36
Eliot Partridge <byte@code.horse>
2020-12-25 14:19:57 -0600

    Tentatively translate into other languages

--- lua/amongus/lang/au_skeld.lua
+++ lua/amongus/lang/au_skeld.lua
@@ -1,7 +1,15 @@
 AddCSLuaFile()
 
-local LANG = GAMEMODE.Lang:Get('en')
+local NAV_HALL_OVERRIDES = {
+	'en': 'Vent to Navigation hallway',
+	'de': 'Zu Navigationsflur venten',
+	'fr': 'Vent to Navigation hallway', -- TODO: change?
+	'ru': 'Телепортироваться в Коридор Навигации',
+	-- 'cn': '潜入到 试点室走廊', -- this is probably terrible and should be reviewed by an actual speaker
+}
 
-LANG['vent.reactorSouth'] = LANG['vent.reactor']
-LANG['vent.navigationSouth'] = LANG['vent.navigation']
-LANG['vent.navHallway'] = LANG['vent.navigation'] .. ' hallway'
+for lang, tab in pairs(GAMEMODE.Lang.__database) do -- dirty hack to enumerate every registered language
+	tab['vent.reactorSouth'] = tab['vent.reactor']
+	tab['vent.navigationSouth'] = tab['vent.navigation']
+	tab['vent.navHallway'] = NAV_HALL_OVERRIDES[lang] or tab['vent.navigation']
+end
