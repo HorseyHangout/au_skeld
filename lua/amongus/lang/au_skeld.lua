837b1517b26aabf57a599c71186b2850146cf72d
Eliot Partridge <byte@code.horse>
2021-03-04 20:34:49 -0600

    Fix Lua errors in translation file

--- lua/amongus/lang/au_skeld.lua
+++ lua/amongus/lang/au_skeld.lua
@@ -1,10 +1,10 @@
 AddCSLuaFile()
 
 local NAV_HALL_OVERRIDES = {
-	'en': 'Vent to Navigation hallway',
-	'de': 'Zu Navigationsflur venten',
-	'fr': 'Vent to Navigation hallway', -- TODO: change?
-	'ru': 'Телепортироваться в Коридор Навигации',
+	['en'] = 'Vent to Navigation hallway',
+	['de'] = 'Zu Navigationsflur venten',
+	['fr'] = 'Vent to Navigation hallway', -- TODO: change?
+	['ru'] = 'Телепортироваться в Коридор Навигации',
 	-- 'cn': '潜入到 试点室走廊', -- this is probably terrible and should be reviewed by an actual speaker
 }
 
